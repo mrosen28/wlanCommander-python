ac4ac26421126aed8ca527c9eb3f2344fca2c44a
Mike Rosenberg <mrosen28@binghamton.edu>
2018-07-26 21:37:15 -0400

    Testing Username

--- cisco.py
+++ cisco.py
@@ -7,7 +7,6 @@ PORT = 23 #Telnet Port
 lowBandChannels = [3,6]
 highBandChannels = [36,38,42,149,151,155]
 
-
 def connect():
     global ap,connected
     ap = telnetlib.Telnet()
@@ -26,23 +25,26 @@ def elevatePrivileges():
     ap.read_until("Password: ")
     enablePassword = getpass.getpass()
     ap.write(enablePassword + "\n")
-    ap.readline()
-    #print ap.read_until("#")
+    print ap.read_until("#")
 
 def setRadioBands(band):
     if band == 2.4:
         print("Enabling: 2.4GHz / Disabling: 5GHz")
         ap.write("ap dot11 5ghz shutdown\n")
+        ap.read_until("#")
         ap.write("no ap dot11 24ghz shutdown\n")
+        ap.read_until("#")
         ap.write("ap dot11 24ghz dot11g")
-
+        ap.read_until("#")
     elif band == 5.0:
         print("Enabling: 5GHz / Disabling: 2.4GHz")
         ap.write("ap dot11 24ghz shutdown\n")
+        ap.read_until("#")
         ap.write("no ap dot11 5ghz shutdown\n")
+        ap.read_until("#")
 
     elif band == None:
-        print("No Band Selected.")
+        print("No Band Selected?")
 
     ap.write("end\n")
 
@@ -51,17 +53,23 @@ def setRadioChannel(channel):
     ap.write("configure terminal\n")
     if channel in lowBandChannels:
         ap.write("interface dot11Radio 0\n") #2.4GHz Radio
+        ap.read_until("#")
     else:
         ap.write("interface dot11Radio 1\n") #5GHz Radio
+        ap.read_until("#")
 
     read_until("#") #Wait for Prompt
     ap.write("channel " + str(channel) + "\n")
+    ap.read_until("#")
     ap.write("end\n")
+    ap.read_until("#")
 
 def setBandWidth(bandwidth):
     print("Setting Channel Bandwidth To: " + str(bandwidth))
     ap.write("config 802.11a chan_width " + APname + " " + bandwidth + "\n")
+    ap.read_until("#")
     ap.write("end\n")
+    ap.read_until("#")
 
 def configAP(select):
     if select < 2:
@@ -85,9 +93,8 @@ def configAP(select):
     elif select == 5 or select == 8:
         setBandWidth(80)
 
-
 def printMenu():
-    print (30 * "-" , "MENU" , 30 * "-")
+    print 30 * "-" , "MENU" , 30 * "-"
     if not connected:
         print ("0. Connect to Controller & Elevate Priveleges")
     else:
@@ -101,17 +108,17 @@ def printMenu():
         print ("7. Test Setup 7: 5GHz - Channel 151 - 40MHz")
         print ("8. Test Setup 8: 5GHz - Channel 155 - 80MHz")
         print ("9. Exit")
-    print (23 * "-" , "Zebra Technologies", 23 * "-")
+    print 23 * "-" , "Zebra Technologies", 23 * "-"
 
 def main():
     while True:
         printMenu()
-        select = int(input("Enter Choice: "))
+        select = int(input("Enter Choice Number: "))
         if select == 0:
             if not connected:
                 print("Connecting...")
                 connect()
-                print("Elevating Privileges...")
+                print("Elevating Privileges... ")
                 elevatePrivileges()
             else:
                 print("Already Connected.")
